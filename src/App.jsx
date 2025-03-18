655ac741b91735581ba2571f5e284a672d79848b
Kiran Jose <Kiran.Jose2@ibm.com>
2025-03-18 15:10:16 +0530

    styling

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import AdminPanel from "./components/AdminPanel/AdminPanel";
 import MovieList from "./components/MovieList/MovieList";
 import VotingForm from "./components/VotingForm/VotingForm";
 import Home from "./components/Home/Home";
+import Navbar from "./components/Navbar/Navbar"; // Import the Navbar component
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <Navbar /> {/* Include the Navbar in the app */}
+      <div className="container mt-4">
         <h1>Voting Application</h1>
         <Routes>
           <Route path="/" element={<Home />} />
